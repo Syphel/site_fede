f0fca499eda4e738716f84312ecd81c3d633336b
GreatToubib <basilburleon@hotmail.com>
2019-09-04 21:57:48 +0200

    ajout dans federale->pv de reunions : upload de pv et affichage des pv + telechargement

--- pages/federale/pv.php
+++ pages/federale/pv.php
@@ -26,34 +26,109 @@
 
 		<?php include("../navbar.php"); ?>
 
-
-		<div class="container"> 
-
-		   		<?php
-
-		   		include ("../../model/dao/cercleDAO.php");
-		   		$cercle=selectByName($bdd,'Fédérale');
-
-				?>
-
-				<div class="margintop marginbottom" >
-
-							
-
-					<p>
-			   			<?php echo $cercle['description_cercle']; ?> <br>
-			   			<br> 
-
-			   		<div align="center">
-			   			<img class= "center" src="<?php echo $cercle['logo_cercle'] ?> ">
-			   		</div>
-
-			  		</p>
-
+		<div class="container"> <!-- upload files -->
+			<div class="container">			
+				<div class="page-header">
+					<h1>Upload PV de réunion </h1>
 				</div>
-
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<form method="post" enctype="multipart/form-data" name="formUploadFile" id="uploadForm" action="uploadpv.php">
+							<div class="form-group">
+								<label for="exampleInputFile">Select files to upload:</label>
+								<input type="file" id="exampleInputFile" name="files[]" multiple="multiple">
+								<p class="help-block"><span class="label label-info">Note:</span> Please, Select the only images (.jpg, .jpeg, .png, .gif) to upload with the size of 100KB only.</p>
+							</div>			
+							<button type="submit" class="btn btn-primary" name="btnSubmit" >Start Upload</button>
+						</form>
+						<br/>
+						<label for="Progressbar">Progress:</label>
+						<div class="progress" id="Progressbar">
+							<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 0%" id="divProgressBar">
+								<span class="sr-only">45% Complete</span>
+							</div>						
+						</div>
+						<div id="status">
+						</div>
+					</div>
+				</div>
+			</div>
+		
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jQuery.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+		
+		<script src="js/jQuery.Form.js"></script>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){			
 				
-		    </div>
+				var divProgressBar=$("#divProgressBar");
+				var status=$("#status");
+				
+				$("#uploadForm").ajaxForm({
+					
+					dataType:"json",
+					
+					beforeSend:function(){
+						divProgressBar.css({});
+						divProgressBar.width(0);
+					},
+					
+					uploadProgress:function(event, position, total, percentComplete){
+						var pVel=percentComplete+"%";
+						divProgressBar.width(pVel);
+					},
+					
+					complete:function(data){
+						status.html(data.responseText);
+					}
+				});
+			});
+		</script>
+
+		</div>
+
+		<div class="container">			
+			<div class="page-header">
+				<h1>Procès verbaux </h1>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<br/>
+					<?php 
+						$conn = mysqli_connect("localhost","root","","bdd_site_fede");
+						
+						$query = "SELECT * FROM pv ORDER BY `pv_name` DESC";
+						
+						$result = mysqli_query($conn, $query);
+						
+						if(mysqli_num_rows($result) > 0)
+						{
+							while($row = mysqli_fetch_assoc($result))
+							{
+								$url = $row["pv_path"]."/".$row["pv_name"];
+					?>
+								<li> <a href="<?php echo $url; ?>" target="_blank"> <?php echo $row["pv_name"]  ?> </a> </li>
+					<?php
+							}
+						}
+						else
+						{
+					?>
+						<p>There are no images uploaded to display.</p>
+					<?php
+						}
+					?>					
+				</div>
+			</div>
+		</div>
+		
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jQuery.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>	
 
 
 		<?php include("../footer.php"); ?>

b8ea130c2e198860892d3ad6b257f14853c0c74c
furtivesquirrel <terry.weber96@outlook.be>
2019-09-05 16:42:02 +0200

    Amélioration page d'accueil

--- pages/index/index.php
+++ pages/index/index.php
@@ -45,43 +45,47 @@ session_start();
 
 		<?php include("../navbar.php"); ?>
 
-		<!-- CAROUSSEL-->
-		<div class="container">
-		  <h2>Événements à venir</h2>  
-		  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-		    <!-- Indicators -->
-		    <ol class="carousel-indicators">
-		      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		      <li data-target="#myCarousel" data-slide-to="1"></li>
-		      <li data-target="#myCarousel" data-slide-to="2"></li>
-		    </ol>
+		
+		<div class="title-area">
+			<h2 class="title">Événements à venir</h2> 
+		  	<span class="title-line"></span> 
+		</div>
+		
+		<!-- CAROUSSEL-->		
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		   <!-- Indicators -->
+			<ol class="carousel-indicators">
+		    	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		    	<li data-target="#myCarousel" data-slide-to="1"></li>
+		     	<li data-target="#myCarousel" data-slide-to="2"></li>
+		   </ol>
 
 		    <!-- Wrapper for slides -->
-		    <div class="carousel-inner">
-		      <div class="item active">
-		        <img src="../../assets/img/Bal180.jpg" alt="Bal">
-		      </div>
+			<div class="carousel-inner">
+			    <div class="item active">
+			    	<img src="../../assets/img/event1.jpg" alt="Bal">
+			    </div>
 
-		      <div class="item">
-		        <img src="../../assets/img/Crasino180.jpg" alt="Crasino">
-		      </div>
-		    
-		      <div class="item">
-		        <img src="../../assets/img/houdain9.jpg" alt="Houdain">
-		      </div>
-		    </div>
+			    <div class="item">
+			    	<img src="../../assets/img/event2.jpg" alt="Crasino">
+			    </div>
+			    
+			    <div class="item">
+			        <img src="../../assets/img/event3.jpg" alt="Houdain">
+			    </div>
+			</div>
 
-		    <!-- Left and right controls -->
-		    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		      <span class="glyphicon glyphicon-chevron-left"></span>
-		      <span class="sr-only">Previous</span>
-		    </a>
-		    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-		      <span class="glyphicon glyphicon-chevron-right"></span>
-		      <span class="sr-only">Next</span>
-		    </a>
-		  </div>
+			    <!-- Left and right controls -->
+			<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			    <span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			    <span class="sr-only">Next</span>
+			</a>
 		</div>
+
 
 
 
